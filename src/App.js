7b24663de8f80964759f43bd7ac2122b6edeffc6
Joshua Richardson <jrichardson@linear-systems.com>
2020-12-18 22:11:34 -0800

    Modified app.js to include the main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import MainPage from './pages/MainPage';
+
 import './App.css';
 
 export default function App() {
   return (
     <div className='App'>
-      <header className='App-header'></header>
+      <header className='App-header'>
+        <MainPage />
+      </header>
     </div>
   );
 }
